18b626b5df142e6937645ec6bd4279e77e74c26e
kodai-siip <wwan55571@gmail.com>
2021-12-15 16:45:05 +0900

    newのプログラミングと画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get "/", to: "top#index"
+
+  get "/folklore/new", to: "folklore#new"
+  get "/folklore/new_category", to: "folklore#new_category"
+  get "/folklore/new_icon", to: "folklore#new_icon"
+  get "/folklore/new_confirm", to: "folklore#new_confirm"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
