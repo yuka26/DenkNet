26cf21325c394c7eaba17ecae92cb5e58fa932f0
kodai-siip <wwan55571@gmail.com>
2021-12-17 10:45:24 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "top#index"
+
+  get "/mypage/mypage", to: "mypage#mypage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
